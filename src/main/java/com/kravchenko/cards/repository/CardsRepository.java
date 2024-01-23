9a6003897c7dda5d3b285276fddaccd76eb566ad
Yehor Kravchenko <brazuca.egor@gmail.com>
2024-01-23 22:45:07 +0200

    DEV: Initial commit

---
+++ src/main/java/com/kravchenko/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.kravchenko.cards.repository;
+
+import com.kravchenko.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
