9576865e08d6401b1ed2fdd91da270f77a4b2645
Yehor Kravchenko <brazuca.egor@gmail.com>
2024-01-27 16:58:52 +0200

    DEV: Add possibility to refresh configurations at runtime using refresh actuator path

--- src/main/java/com/kravchenko/cards/dto/CardsContactInfoDto.java
+++ src/main/java/com/kravchenko/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,17 @@
 package com.kravchenko.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
